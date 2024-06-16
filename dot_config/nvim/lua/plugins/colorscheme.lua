f1c2b29846f5cae86f5671293085037453c94df4
Imran Yafith <imranyafith@gmail.com>
2024-06-16 13:40:14 -0400

    update: italics on colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
-	{
-		"sainnhe/gruvbox-material",
-		priority = 1000,
-		config = function()
-			vim.o.background = "dark"
-			vim.g.gruvbox_material_transparent_background = 1
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      vim.o.background = "dark"
+      vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
