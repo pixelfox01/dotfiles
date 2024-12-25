be7ecfb2cabba9d66e6ef57c662eff930f516ed2
pixelfox01 <imranyafith@gmail.com>
2024-12-25 02:57:12 -0500

    redo initial commit

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,15 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      sections = {
-        lualine_b = {},
-        lualine_x = {
-          "branch",
-          "diff",
-          "diagnostics",
-        },
-      },
-      options = {
-        theme = "gruvbox-material",
-        globalstatus = true,
-        icons_enabled = true,
-        component_separators = "|",
-        section_separators = "|",
-      },
-    })
-  end,
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+	    options = {
+		section_separators = { left = '', right = '' },
+		component_separators = { left = '|', right = '|' },
+		theme = "horizon",
+	    },
+		    sections = {
+			    lualine_c = {},
+			lualine_x = {"filename", "filetype"},
+		    },
+    }
 }
